9a80cac3c02eaae0c10758273e3d1c2aeffde7ed
JunaidHafeez21 <160257992+JunaidHafeez21@users.noreply.github.com>
2024-02-19 16:39:14 +0500

    Add files via upload

---
+++ Q37.js
@@ -0,0 +1,8 @@
+"use strict";
+// 37.Large Shirts: Modify the make_shirt() function so that shirts are large by default with a message that reads I love TypeScript. Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
+function make_shirt(size = "Large", message = "I love TypeScript") {
+    console.log(`T-shirt Summary: Size - ${size}, Message - "${message}"`);
+}
+make_shirt();
+make_shirt("Medium");
+make_shirt("small", "TypeScript is a powerful language!");
